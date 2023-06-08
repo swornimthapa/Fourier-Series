eb5a2d6df430937c59aec0b5a84181265cc4ec0c
swornimthapa <swornimtheking@gmail.com>
2023-06-08 14:29:07 +0545

    added waves

--- src/simulationPanel.java
+++ src/simulationPanel.java
@@ -2,20 +2,22 @@ import Inputs.keyInputs;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 import static java.lang.Math.*;
 
 public class simulationPanel extends JPanel implements Runnable{
     int circleX=0 , circleY=0;
-    double x=0,y=0;
+
     keyInputs keyboradInputs;
-    int radius=0;
+
 
     double time=0;
     double frames=120;
     int numberOFEpicircles=0;
+    ArrayList<Integer> array= new ArrayList<Integer>();
     public simulationPanel(){
-            this.setPreferredSize(new Dimension(900,700));
+            this.setPreferredSize(new Dimension(1500,500));
             this.setDoubleBuffered(true);
 
             keyboradInputs=new keyInputs();
@@ -36,42 +38,55 @@ public class simulationPanel extends JPanel implements Runnable{
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        numberOFEpicircles=20;
+      //  int preCircleX, preCircleY=0;
+        int n=0;
+        int preX=0,preY=0;
+        double x=0,y=0;
+        int radius=0;
+       // preCircleX=circleX;
+       // preCircleY=circleY;
+
         Graphics2D g2d=(Graphics2D) g;
         g2d.translate(200,250);
         g2d.addRenderingHints(new RenderingHints(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON));
-
-       // int radius= (int) (4/PI);
-        numberOFEpicircles=3;
-        int preCircleX, preCircleY=0;
-        int n=0;
-
-        preCircleX=circleX;
-        preCircleY=circleY;
-       // preRadius=radius;
         g.setColor(Color.black);
+
         for(int i=0;i<numberOFEpicircles;i++) {
 
             n=i*2+1;
-            radius= (int) (100 * (4 / (n * PI)));
-            x=(radius *cos(n*time)) ;
-            y=(radius *sin(n*time)) ;
+            radius= (int) (80 * (4 / (n * PI)));
+
+            x+=(radius *cos(n*time)) ;
+            y+=(radius *sin(n*time)) ;
+
+            g.drawOval((int) circleX-radius, (int) circleY-radius, radius*2 , radius*2);
+            g.drawLine((int) (circleX), (int) (circleY), (int) x-preX, (int)y-preY);
 
 
-            g.drawOval((int) preCircleX-radius, (int) preCircleY-radius, radius * 2, radius* 2);
-            g.drawLine((int) (preCircleX), (int) (preCircleY), (int) x, (int)y);
+            g2d.translate(x-preX,y-preY);
+            preX= (int) x;
+            preY= (int) y;
+//            preCircleX=0;
+//            preCircleY= 0;
 
-            g2d.translate(x,y);
-//            preCircleX= (int) x;
-//            preCircleY= (int) y;
 
-            preCircleX=0;
-            preCircleY= 0;
+        }
+        array.add( (int) y);
+         g2d.translate(-x,-y);
+        int capacity=array.size();
+        g.drawLine((int) x, (int) y, (int) 200, (int) array.get(capacity-1));
+        g2d.translate(200,0);
+
+        for(int j=0;j<array.size();j++) {
+
+            g.drawOval((int) j, (int) array.get(j), 1, 1);
+
 
         }
 
-        //g.drawOval((int)circleX,(int)circleY,radius*2,radius*2);
-        time=time+0.02;
 
+        time=time+0.02;
     }
 
 
